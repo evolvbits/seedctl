ab5e5660fc211bbd6cecf037b44705f8c2daebac
William C. Canin <william.costa.canin@gmail.com>
2026-02-24 22:06:09 -0300

    Addning code for Cardano and Monero

--- crates/seedctl-core/src/constants.rs
+++ crates/seedctl-core/src/constants.rs
@@ -1,3 +1,6 @@
 pub const BIP44: u32 = 44u32; // BIP44
 pub const ETHEREUM_COIN_TYPE: u32 = 60u32; // Ethereum coin type
+pub const CARDANO_PURPOSE: u32 = 1852u32; // CIP-1852
+pub const CARDANO_COIN_TYPE: u32 = 1815u32; // ADA (SLIP-44)
+pub const MONERO_COIN_TYPE: u32 = 128u32; // XMR (SLIP-44)
 pub const BITS_PER_DIE: f64 = 2.584962500721156;
